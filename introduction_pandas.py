a1ccfc38e501e458fbcb611ed3005a790553a05a
IshanWankhede <ishan.wankhede7@gmail.com>
2026-02-22 16:28:04 +0530

    Introduction to Pandas

---
+++ introduction_pandas.py
@@ -0,0 +1,23 @@
+# - Pandas is an open-source Python library created by Wes McKinney in 2008.
+# - The name comes from “Panel Data” and “Python Data Analysis”.
+# - It’s widely used in data science, machine learning, finance, 
+# and analytics because it makes working with structured data 
+# (like tables, spreadsheets, or SQL results) simple and efficient.
+
+
+# Key Features
+# • 	Data Structures:
+# • 	"Series" → 1D labeled array (like a column in Excel).
+# • 	"DataFrame" → 2D labeled data structure (like a full table with rows and columns).
+
+# • 	Data Handling:
+# • 	Load data from CSV, Excel, SQL, JSON, etc.
+# • 	Clean messy datasets (handle missing values, duplicates).
+# • 	Transform and reshape data easily.
+# • 	Integration:
+# • 	Works seamlessly with NumPy (numerical ops), Matplotlib/Seaborn 
+# (visualization), SciPy (statistics), and Scikit-learn (machine learning).
+
+import pandas as pd
+
+# print(pd.__version__)
